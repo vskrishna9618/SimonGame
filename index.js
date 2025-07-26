e4d23682adb64bd13f8ed7a2e599a52fd16beab6
Mummareddy Venkata Sai Krishna <144550367+vskrishna9618@users.noreply.github.com>
2025-07-26 16:56:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+var buttonColours = ["red","blue","green","yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+
+var level = 0;
+
+$(document).keypress(function(){
+    if(!started){
+        // $("#level-title").text("Level "+level);
+        nextSequence();
+        started = true;
+    }
+});
+
+function playsound(name){
+    var sound = new Audio("sounds/"+name+".mp3");
+    sound.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    },100);
+}
+
+function nextSequence(){
+
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level "+level);
+
+    var randomNumber = Math.floor((Math.random())*4);
+    var randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playsound(randomChosenColour);
+}
+
+$(".btn").click(function () { 
+
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    animatePress(userChosenColour);
+    playsound(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel){
+    
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("Success");
+        if(gamePattern.length === userClickedPattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+
+    else{
+        console.log("Wrong");
+
+        playsound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");    
+        },200);
+
+        $("#level-title").text("Game Over, Press any Key to Restart");
+
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
